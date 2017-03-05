0f59580e7ac027a2d84e32922bacfecd3cfec828
Algram <aliasgram@gmail.com>
2017-03-05 12:59:43 +0100

    Added maxBuffer: Infinity to fix errors on certain videos

--- src/server/handlers/youtube.js
+++ src/server/handlers/youtube.js
@@ -29,7 +29,7 @@ function download(url, options = {
       // Optional arguments passed to youtube-dl.
       ['--format=18'],
       // Additional options can be given for calling `child_process.execFile()`.
-      { cwd: __dirname });
+      { cwd: __dirname, maxBuffer: Infinity });
 
     // Will be called when the download starts.
     video.on('info', info => {
