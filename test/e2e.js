41c379c89d068cfaf9d6242b4a5f5c69900eb60c
Algram <aliasgram@gmail.com>
2018-09-23 13:50:05 +0000

    Switched to Javascript Standard Style

--- test/e2e.js
+++ test/e2e.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const http = require('http');
-const querystring = require('querystring');
+const http = require('http')
+const querystring = require('querystring')
 
 const postData = querystring.stringify({
   'url': 'https://www.youtube.com/watch?v=qNf9nzvnd1k'
-});
+})
 
 const req = http.request(
   {
@@ -19,23 +19,27 @@ const req = http.request(
     }
   },
   (res) => {
-    const { statusCode } = res;
+    const { statusCode } = res
+
     res.on('data', (chunk) => {
-      console.log(`BODY: ${chunk}`);
-    });
+      console.log(`BODY: ${chunk}`)
+    })
+
     res.on('end', () => {
-      console.log('No more data in response.');
+      console.log('No more data in response.')
+
       if (statusCode !== 200) {
-        console.log(`got status ${statusCode}`);
-        process.exit(1);
+        console.log(`Got status ${statusCode}`)
+        process.exit(1)
       }
-      console.log('yay it works')
-    });
+
+      console.log('Yay it works')
+    })
   }
 ).on('error', (e) => {
-  console.error(`Got error: ${e.message}`);
-  process.exit(1);
-});
+  console.error(`Got error: ${e.message}`)
+  process.exit(1)
+})
 
-req.write(postData);
-req.end();
+req.write(postData)
+req.end()
